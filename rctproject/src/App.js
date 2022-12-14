d1cffd01e258f6800d6b49639e8823fe1bec86da
Abdus Samim <absamim6157@gmail.com>
2022-12-14 23:21:25 +0530

    footer added

---
+++ rctproject/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hellow React </h1>
+
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
